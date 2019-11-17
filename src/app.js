41da2892e0be77964ca8521607a5ef1a1a8f946a
prasanth <shenoy.prasanth@gmail.com>
2019-11-17 11:36:22 +0530

    init commit of ReactWhiteBoard

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h3>proceed with content development here</h3>
+      </div>);
+  }
+}
